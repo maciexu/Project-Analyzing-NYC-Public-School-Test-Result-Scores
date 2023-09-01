df1dff44d929b748b939a1d9ffbf1fc02545218b
maciexu <43970987+maciexu@users.noreply.github.com>
2023-08-31 22:29:49 -0400

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -24,7 +24,36 @@ where average_math >= 640
 order by average_math desc; 
 
 
+-- Task 5. Lowest reading score
+select min(average_reading) as lowest_reading
+from schools; 
+
+-- Task 6. Best writing school
+select school_name, max(average_writing) as max_writing
+from schools
+group by school_name
+order by max_writing desc
+limit 1;
 
 
+-- Task 7. Top 10 schools. Create total SAT scores and find the top 10 best schools.
+select school_name, sum(average_math + average_reading + average_writing) as average_sat
+from schools
+group by school_name
+order by average_sat desc
+limit 10;
 
 
+-- Task 8. Ranking boroughs. We will build a query that calculates the number of schools and the average SAT score per borough!
+select borough, count(school_name) as num_schools, sum(average_math + average_reading + average_writing)/count(schools) as average_borough_sat
+from schools
+group by borough
+order by average_borough_sat desc;
+
+-- Task 9. Brooklyn numbers. For our final query of the database, 
+-- let's focus on Brooklyn, which has 109 schools. We wish to find the top five schools for math performance.
+select school_name, average_math
+from schools
+where borough = 'Brooklyn'
+order by average_math desc
+limit 5;
